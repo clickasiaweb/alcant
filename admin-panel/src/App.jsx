5dca1c84a30864e109d91228008bd621dc6caf51
shivruti5658 <shivruti5658@gmail.com>
2026-03-31 12:45:15 +0530

    Remove category management features from admin panel
     Removed from Sidebar:
    - Category Bulk Upload
    - Categories (upload & management)
    - Subcategories (upload & management)
    - Sub-Subcategories (upload & management)
    - Sub3 Categories
    
     Removed from Routes:
    - All category management routes and imports
    - Category upload/management pages
    
     Kept Essential Features:
    - Dashboard
    - Products (with 3-level category support)
    - Bulk Upload (product bulk upload)
    - Inquiries
    - Content
    
     Result: Clean, focused admin panel with simplified navigation

--- admin-panel/src/App.jsx
+++ admin-panel/src/App.jsx
@@ -3,16 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DashboardPage from './pages/DashboardPage';
 import ProductsPage from './pages/ProductsPage';
 import BulkUploadPage from './pages/BulkUploadPage';
-import CategoryUpload from './components/CategoryUpload';
-import SubcategoryUpload from './components/SubcategoryUpload';
-import SubSubcategoryUpload from './components/SubSubcategoryUpload';
-import Sub3CategoriesUpload from './components/Sub3CategoriesUpload';
 import SimpleDownloadTest from './components/SimpleDownloadTest';
 import SimpleTest from './pages/SimpleTest';
-import CategoriesPage from './pages/CategoriesPage';
-import SubCategoriesPage from './pages/SubCategoriesPage';
-import SubSubCategoriesPage from './pages/SubSubCategoriesPage';
-import Sub3CategoriesPage from './pages/Sub3CategoriesPage';
 import InquiriesPage from './pages/InquiriesPage';
 import ContentPage from './pages/ContentPage';
 import './index.css';
@@ -25,16 +17,8 @@ function App() {
           <Route path="/" element={<DashboardPage />} />
           <Route path="/products" element={<ProductsPage />} />
           <Route path="/bulk-upload" element={<BulkUploadPage />} />
-          <Route path="/category-upload" element={<CategoryUpload />} />
-          <Route path="/subcategory-upload" element={<SubcategoryUpload />} />
-          <Route path="/sub-subcategory-upload" element={<SubSubcategoryUpload />} />
-          <Route path="/sub3-categories-upload" element={<Sub3CategoriesUpload />} />
           <Route path="/test-download" element={<SimpleDownloadTest />} />
           <Route path="/simple-test" element={<SimpleTest />} />
-          <Route path="/categories" element={<CategoriesPage />} />
-          <Route path="/subcategories" element={<SubCategoriesPage />} />
-          <Route path="/sub-subcategories" element={<SubSubCategoriesPage />} />
-          <Route path="/sub3-categories" element={<Sub3CategoriesPage />} />
           <Route path="/inquiries" element={<InquiriesPage />} />
           <Route path="/content" element={<ContentPage />} />
         </Routes>
